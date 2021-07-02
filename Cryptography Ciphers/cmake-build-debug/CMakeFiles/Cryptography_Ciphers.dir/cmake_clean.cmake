908a9b46dafb18d6f71e017393dc43ca847879d7
Abdallah Osama <51860412+abdallah0011@users.noreply.github.com>
2021-07-02 19:05:12 +0200

    Add files via upload

---
+++ Cryptography Ciphers/cmake-build-debug/CMakeFiles/Cryptography_Ciphers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cryptography_Ciphers.dir/main.cpp.obj"
+  "Cryptography_Ciphers.exe"
+  "Cryptography_Ciphers.exe.manifest"
+  "Cryptography_Ciphers.pdb"
+  "libCryptography_Ciphers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cryptography_Ciphers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
